4d87b2bc97d40aeb5907f0a16c0612bcc8a8b2d1
mahmoudtala-at <mahmoudalashkar1@gmail.com>
2023-11-17 14:32:43 +0200

    adding files

---
+++ script/script.js
@@ -0,0 +1,26 @@
+const triggerTabList = document.querySelectorAll('#myTab button')
+triggerTabList.forEach(triggerEl => {
+    const tabTrigger = new bootstrap.Tab(triggerEl)
+
+    triggerEl.addEventListener('click', event => {
+        event.preventDefault()
+        tabTrigger.show()
+    })
+})
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
